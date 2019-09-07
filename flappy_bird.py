67c77b82929df916a5660b07b8c12a5e032fd7dd
lsmucassi <lindasmucassi@gmail.com>
2019-09-07 16:14:06 +0200

    make the flappyBird game first

---
+++ flappy_bird.py
@@ -0,0 +1,18 @@
+import pygame
+import neat
+import time
+import os
+import random
+
+# SCREEN DIMENSIONS
+WIN_HEIGHT = 600
+WIN_WIDTH = 800
+
+#LOAD IMAGES
+BIRD_IMG = [pygame.transform.scale2x(pygame.image.load(os.path.join("imgs", "bird1.png"))),pygame.transform.scale2x(pygame.image.load(os.path.join("imgs", "bird2.png"))),pygame.transform.scale2x(pygame.image.load(os.path.join("imgs", "bird3.png")))] 
+PIPE_IMG = pygame.transform.scale2x(pygame.image.load(os.path.join("imgs", "pipe.png")))
+BASE_IMG = pygame.transform.scale2x(pygame.image.load(os.path.join("imgs", "base.png")))
+BG_IMG = pygame.transform.scale2x(pygame.image.load(os.path.join("imgs", "bg.png")))
+
+#BIRD CLASS
+class Bird:

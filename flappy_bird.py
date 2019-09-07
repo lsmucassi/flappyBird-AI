aff9c28d7e27d036635b1a5bd6f836b68fd2b06a
lsmucassi <lindasmucassi@gmail.com>
2019-09-07 23:32:44 +0200

    more pipes are moving

--- flappy_bird.py
+++ flappy_bird.py
@@ -170,9 +170,11 @@ def draw_window(win, bird, pipes, base):
 def main():
     bird = Bird(230, 350)
     base = Base(730)
-    pipes = [Pipe(700)]
+    pipes = [Pipe(600)]
     win = pygame.display.set_mode((WIN_WIDTH, WIN_HEIGHT))
-    clock = pygame.time.Clock()    
+    clock = pygame.time.Clock()
+
+    score = 0
 
     run = True
     while run:
@@ -182,9 +184,29 @@ def main():
                 run = False
 
         #bird.move()
-        base.move()
+        rem = []
+        add_pipe = False
         for pipe in pipes:
-            pipe.move() 
+            if pipe.collide(bird):
+                pass
+
+            if pipe.x + pipe.PIPE_TOP.get_width() < 0:
+                rem.append(pipe)
+
+            if not pipe.passed and pipe.x < bird.x:
+                pipe.passed = True
+                add_pipe = True
+
+            pipe.move()
+
+        if add_pipe:
+            score += 1
+            pipes.append(Pipe(600))
+
+        for r in rem:
+            pipes.remove(r)
+
+        base.move()
         draw_window(win, bird, pipes, base)
 
     pygame.quit()
